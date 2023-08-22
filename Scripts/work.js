3a9ce9ce786b1ce34fd3ea3e8a1974228deba08a
Robert Zak <roberthelgi@icloud.com>
2023-08-22 07:31:38 +0000

    Finish home page.

---
+++ Scripts/work.js
@@ -0,0 +1,23 @@
+const AUTHOR_OF_WORK = "Róbert Zak";
+const WORK_DATA = [
+
+  {
+    id : 0,
+    work_image : "../Assets/Images/TemplateArt.png",
+    title : "The Mighty Template Art",
+    time_created : "8/22/2023"
+  },
+
+  {
+    id : 1,
+    work_image : "../Assets/Images/TemplateArt.png",
+    title : "The Future Template Art",
+    time_created : "3/3/2024"
+  },
+
+]
+
+function constructHtmlOfWork(work_data)
+{
+  
+}
